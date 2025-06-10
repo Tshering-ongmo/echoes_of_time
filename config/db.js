9a3a2b8494629abe5e3dbb718e842a03e070aa5c
TsheringYangden99 <07230084.sherubtse@rub.edu.bt>
2025-06-10 09:54:19 +0530

    echoes of time

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 const db = pgp({
 host: process.env.DB_HOST || 'localhost', 
-port: 1234, database: process.env.DB_NAME || 'echoes-of_time', 
+port: process.env.DB_PORT || 5432,
+database: process.env.DB_NAME || 'echoes-of_time', 
 user: process.env.DB_USER || 'postgres', 
 password: process.env.DB_PASS || 'bhutanese'
 });
