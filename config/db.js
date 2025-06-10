f576fe16baa07fbe15ca2daf695f2a966786be5a
TsheringYangden99 <07230084.sherubtse@rub.edu.bt>
2025-06-10 10:08:36 +0530

    error

--- config/db.js
+++ config/db.js
@@ -2,11 +2,15 @@ const pgp = require('pg-promise')();
 require('dotenv').config();
 
 const db = pgp({
-host: process.env.DB_HOST || 'localhost', 
-port: process.env.DB_PORT || 5432,
-database: process.env.DB_NAME || 'echoes-of_time', 
-user: process.env.DB_USER || 'postgres', 
-password: process.env.DB_PASS || 'bhutanese'
+host: process.env.DB_HOST, 
+port: process.env.DB_PORT,
+database: process.env.DB_NAME, 
+user: process.env.DB_USER, 
+password: process.env.DB_PASS,
+ssl: {
+  rejectUnauthorized: false // For development only, set to true in production
+}
 });
 
 module.exports = db;
+
