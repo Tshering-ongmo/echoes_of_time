ca412659e5fa1e2178c7312438d26b9da43792c2
TsheringYangden99 <07230084.sherubtse@rub.edu.bt>
2025-06-12 07:56:09 +0530

    final

--- config/db.js
+++ config/db.js
@@ -1,9 +1,6 @@
 const pgp = require('pg-promise')();
 require('dotenv').config();
 
-const db = pgp({
-DATABASE_URL: process.env.DATABASE_URL
-});
+const db = pgp(process.env.DATABASE_URL);
 
 module.exports = db;
-
